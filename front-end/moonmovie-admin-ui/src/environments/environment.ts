83556b944b94ab1ff16a6c88a091240ca93a12bf
Mr-Zero272 <pitithuong@gmail.com>
2024-09-17 18:24:53 +0700

    fix environment file

--- front-end/moonmovie-admin-ui/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    GG_CLIENT_ID: '1023953931483-qq76e1tov9evjr8ai6lfbibpm6mk80ht.apps.googleusercontent.com',
-    GG_CLIENT_SECRET: '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',
-    GG_REDIRECT_URI: 'http://localhost:4200/sign-in',
-};
